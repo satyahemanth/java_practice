9deb95dd4688847c7d93de2feb9df9e8ece6a6f7
satya <satyahemanth53@gmail.com>
2024-01-15 23:45:54 +0530

    Adding git token and selenium automation

---
+++ Selenium_AUTOMATION/src/test/java/package1/ChromeTestWithWebDriverManager.java
@@ -0,0 +1,55 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+class ChromeTestWithWebDriverManager {
+    WebDriver driver;
+    @BeforeTest
+    void setup() {
+        // Set up the wWebDriverManager for chrome driver
+    	WebDriverManager.chromedriver().clearDriverCache().setup();
+        // Create the driver object
+        driver = new ChromeDriver();
+    }
+ 
+    @Test
+    void checkTheUrl() {
+        // Open the browser and go to lambdatest ecommerce website
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+        // Set the current url as a string
+        String url = driver.getCurrentUrl();
+        // Verify that current url contains lambdatest
+        Assert.assertTrue(url.contains("lambdatest"));
+    }
+    @Test
+    void checkTheUrl2() {
+        // Open the browser and go to lambdatest ecommerce website
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+        // Set the current url as a string
+        String url = driver.getCurrentUrl();
+        // Verify that current url contains lambdatest
+        Assert.assertTrue(url.contains("lambdatest"));
+    }
+    @Test
+    void checkTheUrl3() {
+        // Open the browser and go to lambdatest ecommerce website
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+        // Set the current url as a string
+        String url = driver.getCurrentUrl();
+        // Verify that current url contains lambdatest
+        Assert.assertTrue(url.contains("lambdatest"));
+    }
+ 
+    @AfterTest
+    void teardown() {
+       // Close the driver
+        driver.quit();
+    }
+ 
+}
